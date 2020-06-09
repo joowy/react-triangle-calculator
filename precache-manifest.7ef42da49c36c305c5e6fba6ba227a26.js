581cb1c2398d2727b6dca6221c7d50ba8eb50254
DooterMcDooter <46057052+DooterMcDooter@users.noreply.github.com>
2020-06-09 12:24:55 -0400

    Updates

---
+++ precache-manifest.7ef42da49c36c305c5e6fba6ba227a26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b6602e6b7d6936ddb97262764ef4b3",
+    "url": "/react-triangle-calculator/index.html"
+  },
+  {
+    "revision": "eb71699d649e72a2d065",
+    "url": "/react-triangle-calculator/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "510c9a38f115259b949b",
+    "url": "/react-triangle-calculator/static/css/main.12f06b67.chunk.css"
+  },
+  {
+    "revision": "eb71699d649e72a2d065",
+    "url": "/react-triangle-calculator/static/js/2.e66739b5.chunk.js"
+  },
+  {
+    "revision": "6d98c309ab0e4d5aa5d30f35f2390953",
+    "url": "/react-triangle-calculator/static/js/2.e66739b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "510c9a38f115259b949b",
+    "url": "/react-triangle-calculator/static/js/main.ea83f97b.chunk.js"
+  },
+  {
+    "revision": "c932b61ff6f67e84d360",
+    "url": "/react-triangle-calculator/static/js/runtime-main.bd1de83b.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-triangle-calculator/precache-manifest.7ef42da49c36c305c5e6fba6ba227a26.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-triangle-calculator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
